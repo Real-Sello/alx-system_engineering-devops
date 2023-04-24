cfb86fb7c02fcfec4cead7521bc7ff92c009ea0c
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-04-24 13:53:42 +0200

    Delete 0x0A-configuration_management directory
    Removed directory for deferral purposes

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,8 +0,0 @@
-# creating a file
-file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# executing a command
-exec { 'kill-process':
-  command => '/usr/bin/pkill -f killmenow',
-  onlyif  => '/usr/bin/pgrep -f killmenow',
-}
