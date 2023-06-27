1caa39c361028d859b473c5bca7ffaeb4764c8e3
Real-Sello <spmoneatse@gmail.com>
2023-06-27 11:16:19 +0200

    0. Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
-# creating a file
+# Use Puppet to create a file in /tmp
+
 file { '/tmp/school':
-  ensure  => file,
+  ensure  => 'file',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
