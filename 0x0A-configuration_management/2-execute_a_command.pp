37f25c43151217287c0f550603ffdebf3c39ce5c
Real-Sello <spmoneatse@gmail.com>
2023-06-27 11:36:37 +0200

    2. Execute a command|Refactor process termination in Puppet manifest

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,6 @@
 # Using Puppet to create a manifest that kills a process named killmenow
 
 exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => '/usr/bin',
-  onlyif      => 'pgrep -f killmenow',
-  refreshonly => true,
+  command => '/usr/bin/pkill -9 killmenow',
+  onlyif  => '/usr/bin/pgrep killmenow'
 }
