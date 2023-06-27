4dc9e66c98f2f652f9f11206ed24c9f93338c8c8
Real-Sello <spmoneatse@gmail.com>
2023-06-27 13:52:53 +0200

    4. Client configuration file (w/ Puppet)

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+# using Puppet to make changes to our configuration file
+
+file_line { 'PasswdAuth':
+  ensure => 'present',
+  path  => '/etc/ssh/sshd_config',
+  line  => 'PasswordAuthentication no',
+  match => '^#?PasswordAuthentication',
+}
+
+file_line { 'IdentityFile':
+  ensure => 'present',
+  path  => '/etc/ssh/ssh_config',
+  line  => 'IdentityFile ~/.ssh/school',
+  match => '^#?IdentityFile',
+}
