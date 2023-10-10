9486caef25ca1bdd39bf8a15933a977c408259bd
Real-Sello <spmoneatse@gmail.com>
2023-10-10 21:04:32 +0200

    1. Top Ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Function that queries Reddit API
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Function that queries Reddit API and returns
+    the first 10 hot posts listed for a given subreddit
+    """
+    URL = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    post_status = post_status = requests.get(URL,
+                                             headers={'User-agent': 'my-bot'},
+                                             allow_redirects=False)
+
+    if post_status.status_code == 200:
+        posts = post_status.json().get('data').get('children')
+        reader = 0
+        for post in posts:
+            if reader == 10:
+                break
+            print(post.get('data').get('title'))
+            reader += 1
+    else:
+        print('None')
